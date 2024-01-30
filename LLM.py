697750e27760db50db9d23cc76d0910b8c40c3a7
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-01-30 22:52:01 +0100

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -28,7 +28,7 @@ class InternLM_LLM(LLM):
         print("正在从本地加载模型...")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
         #self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
-        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.float16).cuda()  # 测试3班谢文珍的办法
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype=torch.float16).cuda()  # 测试3班谢文珍的办法
         self.model = self.model.eval()
         print("完成本地模型的加载")
 
